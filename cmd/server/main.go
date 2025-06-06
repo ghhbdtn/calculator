2fae6d5e8ab1768ef43b46cad2ad19d223c3accb
Alina Ibryaeva <ibryaevaa@gmail.com>
2025-06-06 18:51:03 +0300

    added readme

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -73,7 +73,6 @@ func startHTTPServer(ctx context.Context, calculator *service.Calculator, errCha
 	// Создаем экземпляр нашего сервера
 	httpServer := httpserver.NewServer(calculator)
 
-	// Graceful shutdown
 	go func() {
 		<-ctx.Done()
 		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
